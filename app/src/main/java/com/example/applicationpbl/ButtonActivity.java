715e2439848a14fef3e6cb097a6c1f78990d9d0e
kabao <te19shibata@g.kumamoto-nct.ac.jp>
2022-11-02 15:21:33 +0900

    予約ボタン追加

--- app/src/main/java/com/example/applicationpbl/ButtonActivity.java
+++ app/src/main/java/com/example/applicationpbl/ButtonActivity.java
@@ -16,7 +16,9 @@ public class ButtonActivity extends AppCompatActivity {
         setContentView(R.layout.activity_button);
     }
 
+    //ボタンを押したときの動作
     public void onClickButton(View view){
+        //電話番号
         Uri number = Uri.parse("tel:1111111111");
         Intent intent;
         intent = new Intent(Intent.ACTION_DIAL, number);
